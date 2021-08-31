d30866c2c752294affe2b33d0e848d5e81a425c7
Rafael <rafael.felipe1989@gmail.com>
2021-08-31 17:31:24 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rafael Ramos on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
