8a321ffd36cc3088dae5d08fc48b27ee90ab6f7d
Rafael <rafael.felipe1989@gmail.com>
2021-09-01 06:53:22 -0300

    created LandmarkViewModel to load file data

---
+++ Landmarks/LandmarkViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Rafael Ramos on 01/09/21.
+//
+
+import Foundation
+
+struct LandmarkViewModel {
+    var bundleHelper: BundleHelperProtocol
+    private let localFileName: String = "landmarkData.json"
+    
+    func getLandmarks() -> [Landmark] {
+        do {
+            return try bundleHelper.load("localFileName", withExtension: "json")
+        } catch {
+            return []
+        }
+    }
+}
