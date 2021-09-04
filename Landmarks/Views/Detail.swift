952e5ceb70d36671e6fc614651768bbcc5747da4
Rafael <rafael.felipe1989@gmail.com>
2021-09-04 17:08:43 -0300

    rename detail to LandmarksDetail

---
+++ Landmarks/Views/Detail.swift
@@ -0,0 +1,50 @@
+//
+//  Detail.swift
+//  Landmarks
+//
+//  Created by Rafael Ramos on 04/09/21.
+//
+
+import SwiftUI
+
+struct LandmarksDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImageView()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
+            }.padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct Detail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarksDetail()
+    }
+}
